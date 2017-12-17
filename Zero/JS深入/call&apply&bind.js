b9677270977cc34137763487f42b288f42007b4d
chester <291164091@qq.com>
2017-12-17 10:07:25 +0800

    bind change

--- Zero/JS深入/call&apply&bind.js
+++ Zero/JS深入/call&apply&bind.js
@@ -1,12 +1,12 @@
 Function.prototype.myCall = function (context) {
     context = context || window;
     context.fn = this;
-    var args = [];
-    for(var i = 1;i<arguments.length;i++){
-        args.push('arguments['+i+']');
+    let args = [];
+    for (let i = 1; i < arguments.length; i++) {
+        args.push('arguments[' + i + ']');
         //args.push(arguments[i])不行是因为在用eval时会将其内容解析为变量，而不是字符串
     }
-    var result = eval('context.fn('+args+')')
+    let result = eval('context.fn(' + args + ')');
     delete context.fn;
     return result;
 };
@@ -14,25 +14,31 @@ Function.prototype.myCall = function (context) {
 Function.prototype.myApply = function (context, arr) {
     context = Object(context) || window;
     context.fn = this;
-    var result;
+    let result;
     if (!arr) {
         return context.fn()
     } else {
-        var args = [];
-        for (var i = 0; i < arr.length; i++) {
+        let args = [];
+        for (let i = 0; i < arr.length; i++) {
             arr.push('arr[' + i + ']');
         }
         result = eval('context.fn(' + args + ')');
     }
     delete context.fn;
     return result;
-}
+};
 
 Function.prototype.myBind = function (context) {
-    var self = this;
-    var args = Array.prototype.slice.call(arguments,1);
-    return function () {
-        var bindArgs = Array.prototype.slice.call(arguments);
-        return self.apply(context,args.concat(bindArgs))
-    }
-}
+    let self = this;
+    let args = Array.prototype.slice.call(arguments, 1);
+    let fNOP = function () {
+    };
+    let fBound = function () {
+        let bindArgs = Array.prototype.slice.call(arguments);
+        return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs))
+    };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP;
+    return fBound
+};
